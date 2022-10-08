3efec76882e2d527d2a6ccef59dd4fd93b31c5d4
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-10-08 18:23:43 -0300

    :tada: Requisito 1
    Adiciona Componentes e Rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './pages/album';
+import ProfileEdit from './pages/editProfile';
+import Favorites from './pages/favoritas';
+import Home from './pages/home';
+import Profile from './pages/profile';
+import Search from './pages/search';
+import NotFound from './pages/notFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Home } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <p>TrybeTunes Home</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
