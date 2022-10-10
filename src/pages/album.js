f36a4fd1b009ef784c9dcbfef906d1895a206d6f
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-10-10 15:40:51 -0300

    :white_check_mark: Requisito 6
    Adiciona Album e novas funcionalidades

--- src/pages/album.js
+++ src/pages/album.js
@@ -1,15 +1,110 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Componentes/header';
+import Load from '../Componentes/loading';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    musics: null,
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.handleSongs();
+  }
+
+  handleSongs = async () => {
+    const { match } = this.props;
+    const request = await getMusics(match.params.id);
+    console.log(request);
+    this.setState({
+      musics: request,
+      loading: false,
+    });
+  };
+
   render() {
+    const { musics, loading } = this.state;
+
     return (
       <div data-testid="page-album">
-        <Header />
-        <p>TrybeTunes Album</p>
+        { loading ? <Load />
+          : (
+            <>
+              <Header />
+              <p>TrybeTunes Album</p>
+              <div>
+                <ul>
+                  { musics.map((musicas) => (
+                    <li
+                      key={ musicas.trackId }
+                      data-testid="album-name"
+                    >
+                      { musicas.wrapperType === 'collection' ? (
+                        <>
+                          <p>
+                            {musicas.artistName}
+                          </p>
+                          <img
+                            key={ musicas.artistName }
+                            src={ musicas.artworkUrl100 }
+                            alt={ musicas.artistId }
+                          />
+
+                        </>
+                      ) : (
+                        <>
+                          <p>
+                            {' '}
+                            {musicas.artistName}
+                            <br />
+                            SongTitle :
+                            {' '}
+                            {musicas.trackName}
+                          </p>
+                          <img
+                            key={ musicas.artistName }
+                            src={ musicas.artworkUrl100 }
+                            alt={ musicas.artistId }
+                          />
+                          <audio
+                            data-testid="audio-component"
+                            src="{previewUrl}"
+                            controls
+                          >
+                            <track kind="captions" />
+                            O seu navegador não suporta o elemento
+                            {' '}
+                            {' '}
+                            <code>audio</code>
+                            .
+                          </audio>
+
+                        </>
+                      ) }
+
+                      <p>
+                        {musicas.collectionName}
+                      </p>
+                      <Link to={ `/album/${musicas.collectionId}` }>
+                        Favorite
+                      </Link>
+                    </li>
+                  )) }
+                </ul>
+              </div>
+
+            </>
+          ) }
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.any,
+}.isRequired;
 
 export default Album;
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Componentes/header';
+import Load from '../Componentes/loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     artistName: '',
+    loading: false,
+    searched: null,
+    searchedArtist: '',
   };
 
   handleArtistName = ({ target }) => {
@@ -13,43 +19,114 @@ class Search extends React.Component {
     });
   };
 
-  render() {
+  handleClick = () => {
     const { artistName } = this.state;
+    this.setState((prevState) => ({
+      searchedArtist: prevState.artistName,
+    }));
+
+    const search = async () => {
+      const request = await searchAlbumsAPI(artistName);
+      this.setState({
+        searched: request,
+      });
+      this.setState({
+        artistName: '',
+        loading: false,
+      });
+    };
+    this.setState({
+      artistName: '',
+      loading: true,
+    }, search);
+  };
+
+  render() {
+    const { artistName, loading, searched, searchedArtist } = this.state;
     const numeroMinimo = 2;
     return (
       <div data-testid="page-search" className="Search">
-        <Header />
-        <p>TrybeTunes Search</p>
-        <form>
-          <label htmlFor="Artist">
-            <input
-              type="text"
-              name="artistName"
-              placeholder="Digite Aqui o nome do Artista"
-              id="Artist"
-              value={ artistName }
-              onChange={ this.handleArtistName }
-              data-testid="search-artist-input"
-            />
-          </label>
-          { artistName.length >= numeroMinimo ? (
-            <button
-              type="button"
-              onClick={ this.handleArtistName }
-              data-testid="search-artist-button"
-            >
-              Pesquisar
-            </button>)
-            : (
-              <button
-                disabled
-                type="button"
-                onClick={ this.handleArtistName }
-                data-testid="search-artist-button"
-              >
-                Pesquisar
-              </button>) }
-        </form>
+        { loading ? <Load />
+          : (
+            <>
+              <Header />
+              <p>TrybeTunes Search</p>
+              <form>
+                <label htmlFor="Artist">
+                  <input
+                    type="text"
+                    name="artistName"
+                    placeholder="Digite Aqui o nome do Artista"
+                    id="Artist"
+                    value={ artistName }
+                    onChange={ this.handleArtistName }
+                    data-testid="search-artist-input"
+                  />
+                </label>
+                {artistName.length >= numeroMinimo ? (
+                  <button
+                    type="button"
+                    onClick={ this.handleClick }
+                    data-testid="search-artist-button"
+                  >
+                    Pesquisar
+                  </button>)
+                  : (
+                    <button
+                      disabled
+                      type="button"
+                      onClick={ this.handleClick }
+                      data-testid="search-artist-button"
+                    >
+                      Pesquisar
+                    </button>)}
+              </form>
+              { searched && searched.length > 0 ? (
+                <>
+                  <p>
+                    {' '}
+                    {`Resultado de álbuns de: ${searchedArtist}`}
+                    {' '}
+                  </p>
+                  <div>
+                    { searched.map((album) => (
+                      <div
+                        key={ album.collectionId }
+
+                      >
+                        <p>
+                          {album.artistName}
+                          {' '}
+                        </p>
+                        <p>
+                          {album.collectionName}
+                        </p>
+                        <Link
+                          to={ `/album/${album.collectionId}` }
+                          data-testid={ `link-to-album-${album.collectionId}` }
+                        >
+                          <img
+                            key={ album.artistName }
+                            src={ album.artworkUrl100 }
+                            alt={ album.artistId }
+                          />
+                        </Link>
+                      </div>
+                    )) }
+                  </div>
+
+                </>
+              ) : searched && (
+
+                <p>
+                  {' '}
+                  Nenhum álbum foi encontrado
+                  {' '}
+                </p>
+              ) }
+            </>
+          ) }
+
       </div>
     );
   }
