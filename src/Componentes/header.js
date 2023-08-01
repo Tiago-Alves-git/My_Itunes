6de73433be4267e4050d823df145a31dd94a92d3
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2023-08-01 11:37:07 -0300

    :white_check_mark: add new header
    new header

--- src/Componentes/header.js
+++ src/Componentes/header.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Avatar, Box, Divider, IconButton, Input, ListItemIcon, Menu,
+  MenuItem, Tooltip } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
 import { getUser } from '../services/userAPI';
 import Load from './loading';
-import image from '../edited.jpg';
+import '../styles/header.css';
 
 class Header extends React.Component {
   state = {
     name: '',
     loading: true,
+    anchorEl: null,
   };
 
   componentDidMount() {
@@ -17,41 +22,119 @@ class Header extends React.Component {
 
   handleUserName = async () => {
     const { name } = await getUser();
-    this.setState({
+    this.setState((prev) => ({
+      ...prev,
       name,
       loading: false,
-    });
+    }));
+  };
+
+  handleClick = (event) => {
+    this.setState((prev) => ({
+      ...prev,
+      anchorEl: event.target,
+    }));
+    console.log(this.state);
+  };
+
+  handleClose = () => {
+    this.setState((prev) => ({
+      ...prev,
+      anchorEl: null,
+    }));
   };
 
   render() {
     // const url = image;
-    const { name, loading } = this.state;
+    const { name, loading, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     return (
       loading ? <Load /> : (
-        <header data-testid="header-component" className="Header">
-          <div className="headerItems">
-            <img
-              src={ image }
-              alt=""
-              className="imageLogo"
+        <div className="Header">
+          <Box sx={ { display: 'flex', alignItems: 'center', textAlign: 'center' } }>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={ this.handleClick }
+                size="small"
+                sx={ { ml: 2 } }
+                aria-controls={ open ? 'account-menu' : undefined }
+                aria-haspopup="true"
+                aria-expanded={ open ? 'true' : undefined }
+              >
+                <MenuIcon sx={ { color: 'black' } } />
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Menu
+            anchorEl={ anchorEl }
+            id="account-menu"
+            open={ open }
+            onClose={ this.handleClose }
+            onClick={ this.handleClose }
+            transformOrigin={ { horizontal: 'right', vertical: 'top' } }
+            anchorOrigin={ { horizontal: 'right', vertical: 'bottom' } }
+          >
+            <MenuItem onClick={ this.handleClose }>
+              <Avatar />
+              {' '}
+              Profile
+            </MenuItem>
+            <MenuItem onClick={ this.handleClose }>
+              <Avatar />
+              {' '}
+              My account
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={ this.handleClose }>
+              <ListItemIcon>
+                <PersonAdd fontSize="small" />
+              </ListItemIcon>
+              Add another account
+            </MenuItem>
+            <MenuItem onClick={ this.handleClose }>
+              <ListItemIcon>
+                <Settings fontSize="small" />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem onClick={ this.handleClose }>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+          <div
+            style={ {
+              backgroundColor: 'white',
+              borderRadius: '10px',
+              display: 'flex',
+              alignItems: 'center',
+              width: '25em',
+            } }
+          >
+            <SearchIcon
+              sx={ {
+                height: '100%',
+                position: 'relative',
+                left: '15em',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: 'black',
+              } }
             />
-            <div>
-              <Link to="/search" data-testid="link-to-search"> Busca </Link>
-              <Link to="/favorites" data-testid="link-to-favorites"> Favoritos </Link>
-              <Link to="/profile" data-testid="link-to-profile"> Perfil </Link>
-            </div>
+            <Input
+              type="text"
+              placeholder="Search..."
+              inputProps={ { 'aria-label': 'search' } }
+              sx={ { width: '100%', marginRight: '20px' } }
+            />
           </div>
-          <div className="headerItems">
-            <h1>
-              Tiago Tunes!
-            </h1>
+          <div style={ { color: 'black' } }>
+            { name }
           </div>
-          <div className="headerItems">
-            <p data-testid="header-user-name">
-              { name }
-            </p>
-          </div>
-        </header>
+        </div>
       )
 
     );
